b26ab0b5972e4ae3dd57a8b11507166851c5246d
Amichai <a>
2013-05-29 19:27:41 -0700

    Many improvements.

--- ModelAndSimulations/CA1D.xaml.cs
+++ ModelAndSimulations/CA1D.xaml.cs
@@ -24,8 +24,35 @@ namespace ModelAndSimulations {
         private bool compare(int a, int b, int c) {
             return inspectionVals[0] == a && inspectionVals[1] == b && inspectionVals[2] == c;
         }
+        int[] transitionRuleArray = new int[8] { 0, 1,1,1,1,0,0,0};
+
+        public int RowLength {
+            get { return _RowLength; }
+            set {
+                if (_RowLength != value) {
+                    _RowLength = value;
+                    OnPropertyChanged(RowLengthPropertyName);
+                }
+            }
+        }
+        private int _RowLength;
+        public const string RowLengthPropertyName = "RowLength";
+
+        public int RectWidth {
+            get { return _RectWidth; }
+            set {
+                if (_RectWidth != value) {
+                    _RectWidth = value;
+                    OnPropertyChanged(RectWidthPropertyName);
+                }
+            }
+        }
+        private int _RectWidth;
+        public const string RectWidthPropertyName = "RectWidth";
+
         private int transitionRule(List<int> vals, int idx, int numberOfStates) {
             ///3 inspection pixels, 8 input types
+            this.transitionRule();
             var layerLength = vals.Count;
             int minus1 = idx - 1 < 0 ? idx - 1 + vals.Count() : idx - 1;
             int plus1 = idx + 1 >= vals.Count() ? idx + 1 - vals.Count() : idx + 1;
@@ -33,21 +60,21 @@ namespace ModelAndSimulations {
             inspectionVals[1] = vals[idx];
             inspectionVals[2] = vals[plus1];
             if (compare(0, 0, 0)) {
-                return 0;    
+                return transitionRuleArray[0];
             } else if (compare(0, 0, 1)) {
-                return 1;
+                return transitionRuleArray[1];
             } else if (compare(0, 1, 0)) {
-                return 1;
+                return transitionRuleArray[2];
             } else if (compare(0, 1, 1)) {
-                return 1;
+                return transitionRuleArray[3];
             } else if (compare(1, 0, 0)) {
-                return 1;
+                return transitionRuleArray[4];
             } else if (compare(1, 0, 1)) {
-                return 0;
+                return transitionRuleArray[5];
             } else if (compare(1, 1, 0)) {
-                return 0;
+                return transitionRuleArray[6];
             } else if (compare(1, 1, 1)) {
-                return 0;
+                return transitionRuleArray[7];
             }
             throw new Exception();
         }
@@ -56,10 +83,12 @@ namespace ModelAndSimulations {
 
         public CA1D() {
             InitializeComponent();
+            this.RectWidth = 5;
+            this.RowSize = 50;
             this.Layers = new List<Layer>();
 
             for (int i = 0; i < 1; i++) {
-                this.Layers.Add(Layer.Generate(NUMBER_OF_STATES, 50, () => rand.Next(5)));
+                this.Layers.Add(Layer.Generate(NUMBER_OF_STATES, RowSize, () => rand.Next(5)));
             }
             system = new CASystem(NUMBER_OF_STATES, this.Layers.First(), transitionRule);
             this.LayersControl.ItemsSource = this.Layers;
@@ -86,9 +115,77 @@ namespace ModelAndSimulations {
         public const string LayersPropertyName = "Layers";
 
         private void Iterate_Click(object sender, RoutedEventArgs e) {
-            this.Layers.Add(system.Iterate());
+            transitionRule();
+            int times = 0;
+            try {
+                times = int.Parse(this.times.Text);
+            } catch {
+                times = 1;
+            }
+            for (int i = 0; i < times; i++) {
+                this.Layers.Add(system.Iterate());
+            }
+            updateLayersUI();
+        }
+
+        private void updateLayersUI() {
             this.LayersControl.ItemsSource = null;
             this.LayersControl.ItemsSource = this.Layers;
+        }
+
+        private void transitionRule() {
+            this.transitionRuleArray[0] = int.Parse(this._000.Text);
+            this.transitionRuleArray[1] = int.Parse(this._001.Text);
+            this.transitionRuleArray[2] = int.Parse(this._010.Text);
+            this.transitionRuleArray[3] = int.Parse(this._011.Text);
+            this.transitionRuleArray[4] = int.Parse(this._100.Text);
+            this.transitionRuleArray[5] = int.Parse(this._101.Text);
+            this.transitionRuleArray[6] = int.Parse(this._110.Text);
+            this.transitionRuleArray[7] = int.Parse(this._111.Text);
+        }
+
+        private void restart() {
+            this.Layers.Clear();
+            for (int i = 0; i < 1; i++) {
+                this.Layers.Add(Layer.Generate(NUMBER_OF_STATES, RowSize, () => rand.Next(5)));
+            }
+
+            system = new CASystem(NUMBER_OF_STATES, this.Layers.First(), transitionRule);
+            updateLayersUI();
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e) {
+            restart();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            try {
+                this.RectWidth = int.Parse((sender as TextBox).Text);
+                (sender as TextBox).Background = Brushes.Green;
+            } catch {
+                (sender as TextBox).Background = Brushes.Red;
+            }
+        }
+
+        public int RowSize {
+            get { return _RowSize; }
+            set {
+                if (_RowSize != value) {
+                    _RowSize = value;
+                    OnPropertyChanged(RowSizePropertyName);
+                }
+            }
+        }
+        private int _RowSize;
+        public const string RowSizePropertyName = "RowSize";
+
+        private void RowSize_TextChanged(object sender, TextChangedEventArgs e) {
+            try {
+                this.RowSize = int.Parse((sender as TextBox).Text);
+                (sender as TextBox).Background = Brushes.Green;
+            } catch {
+                (sender as TextBox).Background = Brushes.Red;
+            }
         }
     }
 }
